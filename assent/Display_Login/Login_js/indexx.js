bb450d1bd2ae1db23a375f5c88209a8d777ce653
Edivan Silva <92985714+Edvan92@users.noreply.github.com>
2023-09-28 14:50:24 -0300

    New  Folder

---
+++ assent/Display_Login/Login_js/indexx.js
@@ -0,0 +1,4 @@
+const valor1 = document.querySelector('.primeiro');
+const valor2 = document.querySelector('.segundo');
+const valor3 = document.querySelector('.terceiro');
+const valor4 = document.querySelector('.quarto');
