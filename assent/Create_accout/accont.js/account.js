c60671049644277034bf52908841f8b6f67a578a
Edivan Silva <92985714+Edvan92@users.noreply.github.com>
2023-09-29 08:58:22 -0300

    AddFolder

---
+++ assent/Create_accout/accont.js/account.js
@@ -0,0 +1,5 @@
+const login = document.querySelector('.login');
+const email = document.querySelectro('.email');
+const pass =   document.querySelectro('.password');
+const passConfirm = document.querySelector('.repeatPass');
+
