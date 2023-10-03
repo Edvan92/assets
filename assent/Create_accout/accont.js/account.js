20de32cfa9ab953477299d298a63d2615452417d
Edivan Silva <92985714+Edvan92@users.noreply.github.com>
2023-10-03 09:55:56 -0300

    Tela cadastro

--- assent/Create_accout/accont.js/account.js
+++ assent/Create_accout/accont.js/account.js
@@ -1,28 +1,44 @@
-const formulary = document.querySelector('.form')
+// Capturando elementos do DOM.
+const formulary = document.querySelector('.form');
 const buttonC = document.querySelector('.buttonC');
 const buttonV = document.querySelector('.buttonV');
 
-function test(){
-//Capturando elementos para manipular no DOM.
-    formulary.addEventListener('submit', function(event){
-        event.preventDefault();
-        
-        const login = document.querySelector('.login').value;
-        const email = document.querySelector('.email').value;
-        const pass =   document.querySelector('.password').value;
-        const passConfirm = document.querySelector('.repeatPass').value; 
-        
-        getData(login, email, pass, passConfirm);
-        
-    });
-}
+// Adicionar um ouvinte de evento para o formulário
+formulary.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-buttonC.addEventListener('click', function(){
-    test();
+  const login = document.querySelector('.login').value;
+  const password = document.querySelector('.password').value;
+
+  getData(login, password);
 });
-function getData(login, email, pass, passConfirm){
-    const addData = [];
-    addData.push(login, email, pass, passConfirm);
-    console.log(addData);
+
+// Adicionar um ouvinte de evento para o botão de criação de conta
+buttonC.addEventListener('click', function () {
+  saveAccount();
+});
+
+// Função para obter dados.
+function getData(login, password) {
+  const data = [login, password];
 }
 
+// Função para salvar conta no localStorage
+function saveAccount() {
+  const login = document.querySelector('.login').value;
+  const password = document.querySelector('.password').value;
+
+  // Recuperar os dados existentes do localStorage ou inicializar uma nova lista
+  const existingData = localStorage.getItem('addData');
+  const dataList = existingData ? JSON.parse(existingData) : [];
+
+  // Adicionar os novos dados à lista
+  dataList.push([login, password]);
+
+  // Salvar a lista atualizada no localStorage
+  localStorage.setItem('addData', JSON.stringify(dataList));
+
+  // Limpar os campos de login e senha após salvar
+  document.querySelector('.login').value = '';
+  document.querySelector('.password').value = '';
+}
