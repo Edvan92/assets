e89385d38ed311617d0f0db79ef5b646982cda31
Edivan Silva <92985714+Edvan92@users.noreply.github.com>
2023-10-02 21:47:04 -0300

    #

--- assent/Create_accout/accont.js/account.js
+++ assent/Create_accout/accont.js/account.js
@@ -1,15 +1,28 @@
 const formulary = document.querySelector('.form')
 const buttonC = document.querySelector('.buttonC');
-const buttonV = document.querySelector('.buttonv');
+const buttonV = document.querySelector('.buttonV');
 
+function test(){
+//Capturando elementos para manipular no DOM.
+    formulary.addEventListener('submit', function(event){
+        event.preventDefault();
+        
+        const login = document.querySelector('.login').value;
+        const email = document.querySelector('.email').value;
+        const pass =   document.querySelector('.password').value;
+        const passConfirm = document.querySelector('.repeatPass').value; 
+        
+        getData(login, email, pass, passConfirm);
+        
+    });
+}
 
-buttonC.addEventListener('click', function(e){
-    e.preventDefault();
-
-    const login = document.querySelector('.login').value;
-    const email = document.querySelectro('.email').value;
-    const pass =   document.querySelectro('.password').value;
-    const passConfirm = document.querySelector('.repeatPass').value;
+buttonC.addEventListener('click', function(){
+    test();
 });
-    
+function getData(login, email, pass, passConfirm){
+    const addData = [];
+    addData.push(login, email, pass, passConfirm);
+    console.log(addData);
+}
 
--- assent/Display_Login/Login_js/indexx.js
+++ assent/Display_Login/Login_js/indexx.js
@@ -1,11 +1,4 @@
+import buttonReturn from '../../Create_accout/accont.js/account.js';
 const form = document.querySelector('.form');
 const button = document.querySelector('.button');
 
-button.addEventListener('click', function(e){
-    e.preventDefault();
-    
-    const login = document.querySelector('.login').value;
-    const pa = document.querySelector('.pa').value;
-
-    alert(login);
-});
